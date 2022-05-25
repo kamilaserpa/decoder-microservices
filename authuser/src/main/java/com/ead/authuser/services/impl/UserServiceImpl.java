80deb6429c0b7b0c4cdbf791f589ce754cb30d4d
Kamila Serpa <kamila.serpa@softplan.com.br>
2022-05-24 22:20:22 -0300

    API REST com Spring Web (MS AuthUser)

--- authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
+++ authuser/src/main/java/com/ead/authuser/services/impl/UserServiceImpl.java
@@ -1,14 +1,34 @@
 package com.ead.authuser.services.impl;
 
+import com.ead.authuser.models.UserModel;
 import com.ead.authuser.repositories.UserRepository;
 import com.ead.authuser.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service // Informa ao Spring que ele deve gerenciar esta classe, e gerar pontos de injeção
 public class UserServiceImpl implements UserService {
 
     @Autowired // Ponto de injeção do Repository utilizando @Autowired
     UserRepository userRepository;
+
+    @Override
+    public List<UserModel> findAll() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public Optional<UserModel> findById(UUID userId) {
+        return userRepository.findById(userId);
+    }
+
+    @Override
+    public void delete(UserModel userModel) {
+        userRepository.delete(userModel);
+    }
 
 }
